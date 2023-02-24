35cfd2db097746691ec47a7fdcdbc6d96a4df75f
Elena Andreeva <aes1999@yandex.ru>
2023-02-24 15:09:38 +0500

    added schema

---
+++ models/card.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: {
+    type: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
