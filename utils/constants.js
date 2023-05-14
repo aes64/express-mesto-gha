385109778bbf84359141918c68f5e6ed6c591bfa
Elena Andreeva <aes1999@yandex.ru>
2023-05-14 19:53:22 +0500

    messages

--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,9 @@
 module.exports = Object.freeze({
-  BAD_REQUEST: 400,
-  NOT_FOUND: 404,
-  INTERNAL_SERVER_ERROR: 500,
-  ACCESS_DENIED: 403,
-  ALREADY_EXIST: 409,
-  UNAUTHORIZED: 401,
+  BAD_REQUEST: 'Некорректный запрос',
+  NOT_FOUND: 'Ничего не смогли найти',
+  INTERNAL_SERVER_ERROR: 'Ошибка сервера',
+  ACCESS_DENIED: 'Нет прав',
+  ALREADY_EXIST: 'Такой пользователь уже есть :(',
+  UNAUTHORIZED: 'Необходимо войти',
   REGEXPHTTP: /^(http(s):\/\/.)[-a-zA-Z0-9:%._+~#=]{2,256}\/[-a-zA-Z0-9:%._+~#=]{2,256}/,
 });
