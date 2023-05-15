22ce00ec3a6da47117944522305a575639a2b768
Elena Andreeva <aes1999@yandex.ru>
2023-05-15 19:57:21 +0500

    try1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,14 +5,14 @@ const errors = require('../utils/constants');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.json(new UnauthorizedError(errors.UNAUTHORIZED));
+    return next(new UnauthorizedError(errors.UNAUTHORIZED));
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    return res.json(new UnauthorizedError(errors.UNAUTHORIZED));
+    return next(new UnauthorizedError(errors.UNAUTHORIZED));
   }
   req.user = payload;
   next();
