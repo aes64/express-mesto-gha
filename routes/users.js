2effafe71db7d1512ef4fb00832cb14f566a015e
Elena Andreeva <aes1999@yandex.ru>
2023-05-15 21:33:06 +0500

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const errors = require('../utils/constants');
 
 const {
   getUsers,
@@ -10,8 +12,21 @@ const {
 
 router.get('/', getUsers);
 router.get('/me', getMe);
-router.get('/:userId', getUserById);
-router.patch('/me/avatar', updateAvatar);
-router.patch('/me', updateUser);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+}), getUserById);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().min(2).pattern(errors.REGEXPHTTP),
+  }),
+}), updateAvatar);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
 
 module.exports = router;
